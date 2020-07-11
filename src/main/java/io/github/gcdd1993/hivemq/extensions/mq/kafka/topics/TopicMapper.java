c8ef19bb9f983d7d9ef2376d1d9fcac6327f2607
高晨 <gaochen@maxtropy.com>
2020-07-11 20:34:38 +0800

    Add Topic mapping SPI

---
+++ src/main/java/io/github/gcdd1993/hivemq/extensions/mq/kafka/topics/TopicMapper.java
@@ -0,0 +1,39 @@
+package io.github.gcdd1993.hivemq.extensions.mq.kafka.topics;
+
+import java.util.Map;
+
+/**
+ * Created by gaochen on 2020/7/11.
+ */
+public interface TopicMapper {
+
+    /**
+     * 获取主题映射关系
+     *
+     * @return <mqtt-topic, kafka-topic>
+     */
+    Map<String, String> getMappings();
+
+    /**
+     * 转换MQTT Topic 到Kafka Topic
+     *
+     * @param topic     MQTT Topic
+     * @param variables 可能存在的参数列表，示例：
+     *                  topic mapping --> /sys/${device-name}/properties/upload
+     *                  mqtt topic --> /sys/device-1/properties/upload
+     *                  kafka topic --> /sys/properties/upload, variables --> {"device-name":"device-1"}
+     */
+    String convertMqttTopic2KafkaTopic(String topic, Map<String, String> variables);
+
+    /**
+     * 转换Kafka Topic到MQTT Topic
+     *
+     * @param topic     Kafka Topic
+     * @param variables 可能存在的参数列表，示例：
+     *                  topic mapping --> /sys/${device-name}/properties/upload
+     *                  kafka topic --> /sys/properties/upload, variables --> {"device-name":"device-1"}
+     *                  mqtt topic --> /sys/device-1/properties/upload
+     */
+    String convertKafkaTopicMqttTopic(String topic, Map<String, String> variables);
+
+}
